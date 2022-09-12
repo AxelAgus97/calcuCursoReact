e4cf39f2a59f7ae608406e963602d1b28c6b8071
agustin <agustincaceres97@gmail.com>
2022-09-12 03:08:48 -0300

    calculadora hecha solo con html y css, tambien tiene cosas que ya habia practicado con para hacer la calculadora antes

---
+++ calcu.js
@@ -0,0 +1,57 @@
+// let uno = document.getElementById('uno').value;
+
+// let uno = document.getElementById
+
+// let uno = document.getElementById
+
+// let uno = document.getElementById
+
+// let uno = document.getElementById
+
+// let uno = document.getElementById
+
+// let uno = document.getElementById
+
+// let uno = document.getElementById
+
+// let uno = document.getElementById
+
+// let uno = document.getElementById
+
+// let uno = document.getElementById
+
+// let uno = document.getElementById
+
+// console.log(uno)
+
+// document.querySelector('#resultado').addEventListener('click', ()=>{
+//     const n1 = parseInt(document.querySelector('#n1').value);
+//     const n2 = parseInt(document.querySelector('#n2').value);
+//     const operacion = document.querySelector('#operacion').value;
+//     let result = null;
+//     if (operacion == '+') {
+//         result = n1 + n2;
+//     }else if (operacion == '-') {
+//         result = n1 - n2;
+//     }else if (operacion == '*') {
+//         result = n1 * n2;
+//     }else if (operacion == '/') {
+//         result = n1 / n2
+//     }
+//     console.log(result);
+//     // let div = document.createElement('div');
+//     // let h2 = document.createElement('h2');
+//     // div.appendChild(h2);
+//     // h2.innerHTML=(result);
+//     let parrafoResult = document.querySelector('#parrafo-result');
+//     parrafoResult.innerHTML = result;
+// })
+
+
+
+
+
+
+
+
+
