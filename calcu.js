3a1423c2f0826a09d8a6e15fcf89b2ae60d07e96
agustin <agustincaceres97@gmail.com>
2022-09-22 10:53:17 -0300

    mejore el codigo de js y cambie el input por un p.

--- calcu.js
+++ calcu.js
@@ -1,57 +1,21 @@
-// let uno = document.getElementById('uno').value;
-
-// let uno = document.getElementById
-
-// let uno = document.getElementById
-
-// let uno = document.getElementById
-
-// let uno = document.getElementById
-
-// let uno = document.getElementById
-
-// let uno = document.getElementById
-
-// let uno = document.getElementById
-
-// let uno = document.getElementById
-
-// let uno = document.getElementById
-
-// let uno = document.getElementById
-
-// let uno = document.getElementById
-
-// console.log(uno)
-
-// document.querySelector('#resultado').addEventListener('click', ()=>{
-//     const n1 = parseInt(document.querySelector('#n1').value);
-//     const n2 = parseInt(document.querySelector('#n2').value);
-//     const operacion = document.querySelector('#operacion').value;
-//     let result = null;
-//     if (operacion == '+') {
-//         result = n1 + n2;
-//     }else if (operacion == '-') {
-//         result = n1 - n2;
-//     }else if (operacion == '*') {
-//         result = n1 * n2;
-//     }else if (operacion == '/') {
-//         result = n1 / n2
-//     }
-//     console.log(result);
-//     // let div = document.createElement('div');
-//     // let h2 = document.createElement('h2');
-//     // div.appendChild(h2);
-//     // h2.innerHTML=(result);
-//     let parrafoResult = document.querySelector('#parrafo-result');
-//     parrafoResult.innerHTML = result;
-// })
 
 
+let resultado1=document.querySelector('#resultadoNum');
+let resultado2= resultado1.innerHTML;
+console.log(resultado2)
+
+let btns = document.querySelectorAll('button');
+let btn= btns.forEach(btn => {
+    btn.addEventListener('click', (e)=>{
+        console.log(btn.value);
+        resultado1.innerHTML= btn.value;
+        
+    })
+});
 
 
-
-
-
+let ope = function operaciones(op, n1, n2) {
+    
+}
 
 
